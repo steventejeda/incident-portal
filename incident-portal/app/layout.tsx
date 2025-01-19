9f4a62afa3aa004bfca0fd44b6ac77938b2c8bcf
Steven Tejeda <stephanymendez@Stephanys-MacBook-Pro.local>
2025-01-19 12:37:34 -0500

    import radix ui

--- incident-portal/app/layout.tsx
+++ incident-portal/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <Theme>
         <NavBar />
         <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
