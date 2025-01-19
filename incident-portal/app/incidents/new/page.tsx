82124c360fb5ff8d9c66827d0412523c7b5b3996
Steven Tejeda <stephanymendez@Stephanys-MacBook-Pro.local>
2025-01-19 16:30:37 -0500

    add markdown editor

--- incident-portal/app/incidents/new/page.tsx
+++ incident-portal/app/incidents/new/page.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+'use client';
+import React from 'react';
+import { Button, TextField } from '@radix-ui/themes';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+import dynamic from 'next/dynamic';
 
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
+
+import "easymde/dist/easymde.min.css";
 
 const NewIncidentPage = () => {
-    return (
-        <div className='max-w-xl space-y-3'>
-            <TextField.Root placeholder='Title'>
-                <TextField.Slot>
-                    <MagnifyingGlassIcon height="16" width="16" />
-                </TextField.Slot>
-            </TextField.Root>
-            <TextArea placeholder="Description" />
-            <Button>Submit a new incident</Button>
-        </div>
-    )
-}
+  return (
+    <div className='max-w-xl space-y-3'>
+      <TextField.Root placeholder='Title'>
+        <TextField.Slot>
+          <MagnifyingGlassIcon height="16" width="16" />
+        </TextField.Slot>
+      </TextField.Root>
+      <SimpleMDE placeholder="Description" />
+      <Button>Submit a new incident</Button>
+    </div>
+  );
+};
 
-export default NewIncidentPage
+export default NewIncidentPage;
