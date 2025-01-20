94154c936c59bd1afe6f48dc7c330ef550beac9f
Steven Tejeda <stephanymendez@Stephanys-MacBook-Pro.local>
2025-01-19 19:34:54 -0500

    implement error handling

--- incident-portal/app/api/incidents/route.ts
+++ incident-portal/app/api/incidents/route.ts
@@ -5,8 +5,8 @@ const prisma = new PrismaClient();
 
 
 const createIncidentSchema = z.object( { 
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, 'Title is required.').max(255),
+    description: z.string().min(1, 'Description is required')
 })
 
 export async function POST(request: NextRequest) { 
--- incident-portal/app/incidents/new/page.tsx
+++ incident-portal/app/incidents/new/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React from 'react';
-import { Button, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import dynamic from 'next/dynamic';
 import axios from 'axios';
@@ -9,39 +9,54 @@ import "easymde/dist/easymde.min.css"
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 
-interface IncidentForm { 
+interface IncidentForm {
     title: string;
     description: string;
 }
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr: false,
+    ssr: false,
 });
 
 const NewIncidentPage = () => {
- const router = useRouter();
- const {register, control, handleSubmit} =  useForm<IncidentForm>();
+    const router = useRouter();
+    const { register, control, handleSubmit } = useForm<IncidentForm>();
+    const [error, setError] = useState('');
 
-  return (
-    <form className='max-w-xl space-y-3' 
-    onSubmit={handleSubmit(async (data) => { 
-        await axios.post('/api/incidents', data);
-        router.push('/incidents');
-    }
-    )}>
-      <TextField.Root placeholder='Title'  {...register('title')}>
-        <TextField.Slot>
-          <MagnifyingGlassIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render = {({ field }) => <SimpleMDE placeholder="Description" {...field} />} 
-      />
-      <Button>Submit a new incident</Button>
-    </form>
-  );
+    return (
+        <div className="max-w-xl">
+            {error && (
+                <Callout.Root color="red" className='mb-5'>
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>
+            )}
+
+            <form
+                className='max-w-xl space-y-3'
+                onSubmit={handleSubmit(async (data) => {
+                    try {
+                        await axios.post('/api/incidents', data);
+                        router.push('/incidents');
+                    }
+                    catch {
+                        setError('An unexpected error occurred');
+                    }
+                }
+                )}>
+                <TextField.Root placeholder='Title'  {...register('title')}>
+                    <TextField.Slot>
+                        <MagnifyingGlassIcon height="16" width="16" />
+                    </TextField.Slot>
+                </TextField.Root>
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+                />
+                <Button>Submit a new incident</Button>
+            </form>
+        </div>
+    );
 };
 
 export default NewIncidentPage;
